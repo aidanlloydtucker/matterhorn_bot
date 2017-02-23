06703cd8518eebec1e46ffaf8dc3d18f3afb07f7
Aidan Lloyd-Tucker <aidanlloydtucker@gmail.com>
2017-02-23 15:09:34 -0800

    replaced chatid command with msgdump command

---
+++ commands/msgdump.go
@@ -0,0 +1,43 @@
+package commands
+
+import (
+	"gopkg.in/telegram-bot-api.v4"
+	"encoding/json"
+)
+
+type MsgDumpHandler struct {
+}
+
+var msgDumpHandlerInfo = CommandInfo{
+	Command:     "msgdump",
+	Args:        "",
+	Permission:  3,
+	Description: "dumps the info recieved in the message",
+	LongDesc:    "",
+	Usage:       "/msgdump",
+	Examples: []string{
+		"/msgdump",
+	},
+	ResType: "message",
+}
+
+func (h MsgDumpHandler) HandleCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message, args []string) {
+	var msg tgbotapi.MessageConfig
+
+	jsonBytes, err := json.MarshalIndent(message, "", "	")
+	if err != nil {
+		msg = NewErrorMessage(message.Chat.ID, err)
+	} else {
+		msg = tgbotapi.NewMessage(message.Chat.ID, string(jsonBytes))
+	}
+
+	bot.Send(msg)
+}
+
+func (h MsgDumpHandler) Info() *CommandInfo {
+	return &msgDumpHandlerInfo
+}
+
+func (h MsgDumpHandler) HandleReply(message *tgbotapi.Message) (bool, string) {
+	return false, ""
+}
